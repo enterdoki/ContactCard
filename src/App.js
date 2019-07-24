32c14d8f9d65e9b9c59969eb22d9fb4d8e0af961
Toms <chen.tommy925@gmail.com>
2019-07-23 22:25:47 -0400

    axios fetch crypto api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ContactCard from "./Components/ContactCard/ContactCard";
 import Decrement from "./Components/Decrement/Decrement";
 import Form from "./Components/Form/Form";
 import Folder from "./Components/Folder/Folder";
+import Tickers from "./Components/Tickers/Tickers";
 import './App.css';
 
 class App extends Component {
@@ -18,12 +19,17 @@ class App extends Component {
   render () {
       return (
         <div className = "App">
+          <div className = "App-header">
+            <h2>Cryptocurrency Tickers</h2>
+          </div>
+          <Tickers/>
           <div className = "Title">
             <Typography variant="h3" color="primary">
               Folder
             </Typography>
           </div>
           <div className = "Edit">
+            <Form first = "John" last = "Doe"/>
             <Folder/>
           </div>
           
